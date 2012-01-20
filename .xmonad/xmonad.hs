27c1fcf9a53019b90d131d840c74cfb1480f18b3
Noon Silk <noonsilk@gmail.com>
2012-01-20 13:58:27 +1100

    Comments

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,7 @@
 -- XMonad config, based on Loki Davison's: https://github.com/loki42
+-- This is a simple config with a focus on being able to switch between
+-- layouts easily.
+--
 -- Author: Noon Silk
 -- Location: http://github.com/silky/dotfiles
 
@@ -23,26 +26,22 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 -- Layout Management
+--
 --  We are interested in changing layouts in two ways. One is the
 --  typical iteration with mod-space, the other is specific layout
 --  selection through another-mod-<letter>, where the letter the
 --  specific layout
 
 myLayout = toggleLayouts Full (Tall 1 (3/100) (1/2))
-
--- This does changing between layouts. The mappings are obvious, the
--- potential layouts to consider are:
---  > Full
---  > Tall
---  > ...
---
-myKeys = [
+myKeys   = [
     -- mod4Mask is the windows key.
      ((mod4Mask, xK_f), sendMessage (Toggle "Tall"))
    , ((mod4Mask, xK_g), sendMessage (Toggle "Full"))
   ]  
 
 
+-- Setup
+--
 main = xmonad $ defaultConfig {
     borderWidth            = 1
     , terminal             = "/usr/bin/konsole"
