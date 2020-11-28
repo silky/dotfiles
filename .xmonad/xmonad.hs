7c216b5569777ea63acb2f58eb127d1673147673
Noon van der Silk <noon.vandersilk@riverlane.com>
2020-11-28 17:05:27 +0000

    xmobar wip

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,32 +12,36 @@
 --  https://bitbucket.org/tobyodavies/shared/src
 --  https://betweentwocommits.com/blog/xmonad-layouts-guide
 
+-- TODO:
+--  - Add i3lock
+--
 import System.IO
 import XMonad hiding ( (|||) )
-import XMonad.Hooks.SetWMName
+import XMonad.Actions.CopyWindow
+import XMonad.Actions.CycleWS
+import XMonad.Actions.UpdatePointer
+import XMonad.Actions.WindowBringer
+import XMonad.Actions.WindowGo
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeWindows
 import XMonad.Hooks.Place
-import XMonad.Layout.Fullscreen
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Spiral
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.Circle
-import XMonad.Layout.OneBig
+import XMonad.Layout.Fullscreen
 import XMonad.Layout.Grid
-import XMonad.Layout.ZoomRow
-import XMonad.Layout.LayoutCombinators
-import XMonad.Layout.Named (named)
-import XMonad.Layout.ToggleLayouts
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig
-import XMonad.Actions.WindowBringer
-import XMonad.Actions.WindowGo
-import XMonad.Actions.CopyWindow
-import XMonad.Actions.UpdatePointer
-import XMonad.Actions.CycleWS
 import XMonad.Layout.Groups.Examples
+import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.MultiColumns
+import XMonad.Layout.Named (named)
+import XMonad.Layout.NoBorders
+import XMonad.Layout.OneBig
+import XMonad.Layout.Spiral
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.ZoomRow
+import XMonad.Util.EZConfig
+import XMonad.Util.Run (spawnPipe)
 import TwoBig
 
 import qualified XMonad.StackSet as W
@@ -50,7 +54,6 @@ import qualified Data.Map        as M
 --  typical iteration with mod-space, the other is specific layout
 --  selection through windows-<letter>, where the letter indicates
 --  specific layout
-
 
 myLayout = layoutHints $ smartBorders $ 
         named "Tiled"          tiled 
@@ -81,6 +84,12 @@ myLayout = layoutHints $ smartBorders $
 
 
 layoutChangeModMask = mod1Mask .|. shiftMask
+
+
+myStatusBar = "xmobar -x0 /home/noon/dev/dotfiles/xmobar.conf"
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_q)
 
 
 myKeys   = 
@@ -118,30 +127,93 @@ myMouseBindings x = M.fromList $
     , ((myMouseMod, 9), const $ moveTo Next NonEmptyWS)
     ]
 
+base03  = "#002b36"
+base02  = "#073642"
+base01  = "#586e75"
+base00  = "#657b83"
+base0   = "#839496"
+base1   = "#93a1a1"
+base2   = "#eee8d5"
+base3   = "#fdf6e3"
+yellow  = "#b58900"
+orange  = "#cb4b16"
+red     = "#dc322f"
+magenta = "#d33682"
+violet  = "#6c71c4"
+blue    = "#268bd2"
+cyan    = "#2aa198"
+green   = "#859900"
+
+active       = blue
+activeWarn   = red
+inactive     = base02
+focusColor   = blue
+unfocusColor = base02
+
+-- sizes
+gap    = 10
+topbar = 10
+border = 0
+prompt = 20
+status = 20
+
 
 -- Setup
-main = xmonad $ ewmh def {
-      borderWidth        = 1
-    , terminal           = "/usr/bin/konsole"
-    , normalBorderColor  = "#000000"
-    , focusedBorderColor = "#b141f2"
-    , layoutHook         = myLayout
-    , mouseBindings      = myMouseBindings
-    , modMask            = mod1Mask
+main = do
+  
+  -- TODO: https://hackage.haskell.org/package/xmonad-contrib-0.16/docs/src/XMonad.Hooks.DynamicLog.html#xmobar
+  -- https://xmobar.org/
+  -- https://hackage.haskell.org/package/xmonad-contrib-0.16/docs/XMonad-Hooks-DynamicLog.html
+  -- https://github.com/altercation/dotfiles-tilingwm/tree/31e23a75eebdedbc4336e7826800586617d7d27d
+  -- https://github.com/altercation/dotfiles-tilingwm/blob/31e23a75eebdedbc4336e7826800586617d7d27d/.xmonad/xmonad.hs
+  -- https://www.reddit.com/r/xmonad/comments/wu2ev/toggle_xmobar_visibility_with_a_hotkey/
+  --   (Maybe this is good.)
+  -- http://hackage.haskell.org/package/xmobar-0.8/src/README.html
+  -- https://www.reddit.com/r/xmonad/comments/fgyzc/xmonadactionsdynamicworkspaces_where_have_you/
+  -- https://github.com/polybar/polybarhttps://github.com/polybar/polybar
+  -- https://www.reddit.com/r/xmonad/comments/jyoaat/is_it_possible_to_put_a_repl_in_a_prompt/
+  -- https://github.com/nwf/xconfig
+  -- h <- spawnPipe myStatusBar
 
-    -- Update pointer to be in the center on focus; I tried
-    -- it being the 'Nearest' option, but this was not good
-    -- because it still contains the bug wherein you shift
-    -- to a new window and focus doesn't change.
-    --
-    -- Note: This breaks in GIMP for various reasons that I
-    -- don't care to investigate right now. If you have troubles
-    -- there, just comment it out (or fix it and tell me!).
-    --
-    , logHook            = updatePointer (0.5, 0.5) (0, 0)
+  let myConfig = ewmh def {
+          borderWidth        = 1
+        , terminal           = "/usr/bin/konsole"
+        , normalBorderColor  = "#000000"
+        , focusedBorderColor = "#b141f2"
+        , layoutHook         = myLayout
+        , mouseBindings      = myMouseBindings
+        , modMask            = mod1Mask
+        -- Update pointer to be in the center on focus; I tried
+        -- it being the 'Nearest' option, but this was not good
+        -- because it still contains the bug wherein you shift
+        -- to a new window and focus doesn't change.
+        --
+        -- Note: This breaks in GIMP for various reasons that I
+        -- don't care to investigate right now. If you have troubles
+        -- there, just comment it out (or fix it and tell me!).
+        --
+        , logHook            = do
+          --dynamicLogWithPP $ def
+          --  { ppCurrent             = xmobarColor active "" . wrap "[" "]"
+          --  , ppTitle               = xmobarColor active "" . shorten 50
+          --  , ppVisible             = xmobarColor base0  "" . wrap "(" ")"
+          --  , ppUrgent              = xmobarColor red    "" . wrap " " " "
+          --  , ppHiddenNoWindows     = const ""
+          --  , ppSep                 = xmobarColor red blue "  :  "
+          --  , ppWsSep               = " "
+          --  , ppLayout              = xmobarColor yellow ""
+          --  , ppOrder               = id
+          --  , ppOutput              = hPutStrLn h  
+          --  -- , ppSort                = fmap 
+          --  --                           (namedScratchpadFilterOutWorkspace.)
+          --  --                           (ppSort def)
+          --                            --(ppSort defaultPP)
+          --  , ppExtras              = [] }
+          updatePointer (0.5, 0.5) (0, 0)
+      } `additionalKeys` myKeys `additionalKeysP` [
+            ("M-g", gotoMenu)
+          , ("M-b", bringMenu)
+          , ("M-0", spawn "notify-send \"`echo \\`date '+%I:%M %p %A, %b %d %Y'\\``\"")
+          ]
 
-} `additionalKeys` myKeys `additionalKeysP` [
-      ("M-g", gotoMenu)
-    , ("M-b", bringMenu)
-    , ("M-0", spawn "notify-send \"`echo \\`date '+%I:%M %p %A, %b %d %Y'\\``\"")
-    ]
+  xmonad myConfig
