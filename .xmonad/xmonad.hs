e1e7cddfd20ec4febcb7266f909e02c152c78478
silky <noonsilk+-noonsilk@gmail.com>
2021-02-22 10:52:41 +0000

    cleanup; random image feature makes a comeback.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,9 +12,6 @@
 --  https://bitbucket.org/tobyodavies/shared/src
 --  https://betweentwocommits.com/blog/xmonad-layouts-guide
 
--- TODO:
---  - Add i3lock
---
 import System.IO
 import XMonad hiding ( (|||) )
 import XMonad.Actions.CopyWindow
@@ -77,19 +74,7 @@ myLayout = layoutHints $ smartBorders $
      delta   = 2/100
 
 
--- toggleLayouts is some function that takes two layouts,
--- it's sadly not a setting. Oh well. This config doesn't
--- toggle, but at least it's possible to change to specific
--- layouts.
-
-
 layoutChangeModMask = mod1Mask .|. shiftMask
-
-
-myStatusBar = "xmobar -x0 /home/noon/dev/dotfiles/xmobar.conf"
-
--- Key binding to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_q)
 
 
 myKeys   = 
@@ -102,20 +87,22 @@ myKeys   =
    , ((mod1Mask, xK_o), spawn "nautilus --no-desktop")
    , ((mod1Mask, xK_m), spawn "konsole -e alsamixer")
    , ((mod1Mask, xK_e), spawn "konsole -e nvim")
+   --
+   -- Show a random image full-screen
+   , ((mod1Mask, xK_i), spawn "feh -Z -. --randomize --image-bg black /home/noon/images"
+                        >> sendMessage (JumpToLayout "Full")
+     )
+   -- 
+   -- Move mouse focus to the other screen; useful for more a setup with more
+   -- than one screen
    , ((mod1Mask, xK_q), screenWorkspace 0 >>= flip whenJust (windows . W.view))
    , ((mod1Mask, xK_r), screenWorkspace 1 >>= flip whenJust (windows . W.view))
    --
    -- Flameshot: <https://github.com/lupoDharkael/flameshot>
-   -- 
    , ((mod1Mask, xK_s), spawn "flameshot gui")
    , ((mod1Mask .|. shiftMask, xK_s), spawn "flameshot gui -p ~/Pictures/Screenshots/")
    --
-   -- Used to copy say VLC to other screens to watch movies
-   , ((layoutChangeModMask, xK_v), windows copyToAll)
-   , ((layoutChangeModMask, xK_d), killAllOtherCopies)
-   -- 
    -- Increase the size occupied by the focused window
-   --
    , ((layoutChangeModMask, xK_plus),  sendMessage zoomIn)
    , ((layoutChangeModMask, xK_minus), sendMessage zoomOut)
   ]
@@ -128,54 +115,9 @@ myMouseBindings x = M.fromList $
     , ((myMouseMod, 9), const $ moveTo Next NonEmptyWS)
     ]
 
-base03  = "#002b36"
-base02  = "#073642"
-base01  = "#586e75"
-base00  = "#657b83"
-base0   = "#839496"
-base1   = "#93a1a1"
-base2   = "#eee8d5"
-base3   = "#fdf6e3"
-yellow  = "#b58900"
-orange  = "#cb4b16"
-red     = "#dc322f"
-magenta = "#d33682"
-violet  = "#6c71c4"
-blue    = "#268bd2"
-cyan    = "#2aa198"
-green   = "#859900"
-
-active       = blue
-activeWarn   = red
-inactive     = base02
-focusColor   = blue
-unfocusColor = base02
-
--- sizes
-gap    = 10
-topbar = 10
-border = 0
-prompt = 20
-status = 20
-
 
 -- Setup
 main = do
-  
-  -- TODO: https://hackage.haskell.org/package/xmonad-contrib-0.16/docs/src/XMonad.Hooks.DynamicLog.html#xmobar
-  -- https://xmobar.org/
-  -- https://hackage.haskell.org/package/xmonad-contrib-0.16/docs/XMonad-Hooks-DynamicLog.html
-  -- https://github.com/altercation/dotfiles-tilingwm/tree/31e23a75eebdedbc4336e7826800586617d7d27d
-  -- https://github.com/altercation/dotfiles-tilingwm/blob/31e23a75eebdedbc4336e7826800586617d7d27d/.xmonad/xmonad.hs
-  -- https://www.reddit.com/r/xmonad/comments/wu2ev/toggle_xmobar_visibility_with_a_hotkey/
-  --   (Maybe this is good.)
-  -- http://hackage.haskell.org/package/xmobar-0.8/src/README.html
-  -- https://www.reddit.com/r/xmonad/comments/fgyzc/xmonadactionsdynamicworkspaces_where_have_you/
-  -- https://github.com/polybar/polybarhttps://github.com/polybar/polybar
-  -- https://www.reddit.com/r/xmonad/comments/jyoaat/is_it_possible_to_put_a_repl_in_a_prompt/
-  -- https://github.com/nwf/xconfig
-  -- h <- spawnPipe myStatusBar
-
   let myConfig = ewmh def {
           borderWidth        = 1
         , terminal           = "/usr/bin/konsole"
@@ -192,25 +134,7 @@ main = do
         -- Note: This breaks in GIMP for various reasons that I
         -- don't care to investigate right now. If you have troubles
         -- there, just comment it out (or fix it and tell me!).
-        --
-        , logHook            = do
-          --dynamicLogWithPP $ def
-          --  { ppCurrent             = xmobarColor active "" . wrap "[" "]"
-          --  , ppTitle               = xmobarColor active "" . shorten 50
-          --  , ppVisible             = xmobarColor base0  "" . wrap "(" ")"
-          --  , ppUrgent              = xmobarColor red    "" . wrap " " " "
-          --  , ppHiddenNoWindows     = const ""
-          --  , ppSep                 = xmobarColor red blue "  :  "
-          --  , ppWsSep               = " "
-          --  , ppLayout              = xmobarColor yellow ""
-          --  , ppOrder               = id
-          --  , ppOutput              = hPutStrLn h  
-          --  -- , ppSort                = fmap 
-          --  --                           (namedScratchpadFilterOutWorkspace.)
-          --  --                           (ppSort def)
-          --                            --(ppSort defaultPP)
-          --  , ppExtras              = [] }
-          updatePointer (0.5, 0.5) (0, 0)
+        , logHook            = updatePointer (0.5, 0.5) (0, 0)
       } `additionalKeys` myKeys `additionalKeysP` [
             ("M-g", gotoMenu)
           , ("M-b", bringMenu)
